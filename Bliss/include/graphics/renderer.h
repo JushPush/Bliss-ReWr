b33b232b15a3cd858ea8e1fbde25675c70d7680d
EinKara <ironmasterz@outlook.com>
2023-03-21 10:58:55 -0400

    yeag

--- Bliss/include/graphics/renderer.h
+++ Bliss/include/graphics/renderer.h
@@ -6,11 +6,16 @@ namespace Punji::Graphics {
 	class Renderer {
 	public:
 		Renderer();
+		
+		void SetPixel();
+
 		~Renderer();
 
 		void SetCamera(Camera* camera);
 		Camera* getCamera() { return _camera; }
 	private:
 		Camera* _camera;
+
+		void ResizeBuffer() {}
 	};
 }
